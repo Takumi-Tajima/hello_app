dbece3bb1b2f48faf822cd132a4085d141080343
taji <tajima.takumi0421@gmail.com>
2023-08-15 01:11:04 +0000

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
+  def goodbye
+    render html: "Goodbye, World!"
+  end
   def hello
-    render html: "hola, mundo!"
+    render html: "Hello, World!"
   end
 end
